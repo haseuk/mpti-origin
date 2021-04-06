a2482b6155a47814708e0040e8c2b28d239b862f
haseuk <73511954+haseuk@users.noreply.github.com>
2021-04-06 19:38:50 +0900

    4.6

--- src/components/Choice.vue
+++ src/components/Choice.vue
@@ -4,8 +4,10 @@
       <img src="/img/choice.png" alt="">
       <div class="list-box">
         <ul>
-          <li v-for="(q, month) in surveys" :key="month">
-            <RouterLink v-if="q" :to="`/survey/${month}`" :class="{complete: checkComplete(month)}"></RouterLink>
+          <li v-for="(q, month) in surveys" :key="month" :class="{reverse: reverseImg === month}">
+            <div class="front img"></div>
+            <div class="back img"></div>
+            <a v-if="q" :class="{complete: checkComplete(month)}" @click="choice(month)"></a>
             <a v-else @click="notYet(month)"></a>
           </li>
         </ul>
@@ -22,7 +24,8 @@ export default {
 
   data() {
     return {
-      surveys
+      reverseImg: null,
+      surveys,
     }
   },
   methods: {
@@ -32,6 +35,10 @@ export default {
     checkComplete(month) {
       return this.$store.state.completeSurvey[month];
     },
+    choice(v) {
+      this.reverseImg = v;
+      setTimeout(() => {this.$router.push(`/survey/${v}`)}, 2000);
+    }
   }
 
 }
@@ -43,8 +50,12 @@ export default {
   [choice] {  .abs; .lt;
     .list-box { .wf; .abs; .lt(0,328); .p(0,32,0,25); .-box;
       ul { .flex; flex-direction: row; justify-content: space-evenly;
-        li { .wh(430,430); .tc;
-          a { .f; .block; }
+        li { .wh(430,468); .tc; transition: 1s; .rel; -webkit-transform-style: preserve-3d; -moz-transform-style: preserve-3d; -ms-transform-style: preserve-3d; -o-transform-style: preserve-3d;
+          .img { .ib; .f; .abs; .lt; backface-visibility: hidden; }
+          .front { .contain('/img/choice-4.png'); transform: rotateY(180deg); }
+          .back { .contain('/img/coming.png'); }
+          &.reverse {  transform: rotateY(180deg); }
+          a { .rel; .f; .block; }
         }
       }
     }
